1639412cc6cdb659a872a6f2fdc779e434a70895
lorenzo26 <benedict.bueno26@gmail.com>
2019-01-20 03:41:58 +0800

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -385,7 +385,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
@@ -526,5 +526,3 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-
-$config['sess_save_path'] = sys_get_temp_dir();
